7ae07897de4c2449b37d793e498582e32379d3e2
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-08-26 00:04:10 -0700

    Adds prototype to add up cost of one pizza order

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,45 @@ function Pizza(size){
   this.premium = [];
 }
 
-// Pizza.prototype.finalPrice = function(){
-//   return this.size + (this.standard.length * 0.5) + (this.premium.length);
-// }
+Pizza.prototype.pizzaPrice = function(){
+  var total = 0;
+  if (this.size === "Small"){
+    total += 6;
+  } else if (this.size === "Medium"){
+    total += 7;
+  } else {
+    total += 8;
+  }
+  return (total + (this.standard.length * 0.5) + (this.premium.length)).toFixed(2);
+}
+
+// function totalPrice
 
 // Frontend Logic
+function appendOrder(size, standard, premium, total){
+  $("ul").append("<li>Pizza Size: " + size + "; " + "Standard Toppings: " + standard.toString() + "; Premium Toppings: " + premium.toString() + "Pizza Total: $" + total + "</li>");
+}
+
+
 $("button#addOrder").click(function(){
   var pizzaSize = $("#size").val();
   var standardToppings = [];
   var premiumToppings = [];
-    $("input:checkbox[name=standard]:checked").each(function(){
-      standardToppings.push($(this).val());
-    });
-    $("input:checkbox[name=premium]:checked").each(function(){
-      premiumToppings.push($(this).val());
-    });
   var pizzaOrder = new Pizza(pizzaSize);
+  $("input:checkbox[name=standard]:checked").each(function(){
+    standardToppings.push($(this).val());
+  });
+  $("input:checkbox[name=premium]:checked").each(function(){
+    premiumToppings.push($(this).val());
+  });
   for (var i = 0; i < standardToppings.length; i++){
     pizzaOrder.standard.push(standardToppings[i]);
   }
-  console.log(pizzaOrder.standard);
-  for (var i = 0; i < premiumToppings.length; i++){
-    pizzaOrder.premium.push(premiumToppings[i]);
+  for (var j = 0; j < premiumToppings.length; j++){
+    pizzaOrder.premium.push(premiumToppings[j]);
   }
-  console.log(pizzaOrder.premium);
+  appendOrder(pizzaOrder.size, pizzaOrder.standard, pizzaOrder.premium, pizzaOrder.pizzaPrice());
+  // $(".total").text(totalPrice());
 
 
 });
